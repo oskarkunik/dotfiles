e4867c3c4382a599925d98ac9cf0e15db7fd2dd2
Oskar Kunik <oskar.kunik@gmail.com>
2020-01-22 09:22:19 +0100

    Expand setup script

--- setup.sh
+++ setup.sh
@@ -2,6 +2,17 @@
 bold=$(tput bold)
 normal=$(tput sgr0)
 
+printf "${bold}Install updates\n" &&
+sudo apt update && sudo apt upgrade -y &&
+
+printf "${bold}Install software\n" &&
+sudo apt install python-pip3 zshrc chromium-browser firefox picard &&
+
+
+# .zshrc
+printf "${bold}Deleting ~/.zshrc if exists.\n" &&
+rm -f ~/.zshrc &&
 printf "${bold}Symlinking .zshrc: ${normal}" &&
-ln -sv .zshrc ~ &&
-printf "\nDone."
+ln -sv $HOME/dotfiles/.zshrc $HOME/.zshrc &&
+
+printf "\nDone.\n";
